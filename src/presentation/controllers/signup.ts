88bffbcaff3961f1252658145e3ef3d21cc3cb9e
Giovani Diccini <giovani.diccini@gmail.com>
2022-04-23 15:35:00 -0300

    refactor: make SingUpController conforms to a protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
 import { HttpRequest, HttpResponse } from '../protocols/http';
 import MissingParamError from '../error/missing-param-error';
 import badRequest from '../helpers/http-helper';
+import { Controller } from '../protocols/controller';
 
-export default class SignUpController {
+export default class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse | undefined {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
     // eslint-disable-next-line no-restricted-syntax
